abb354b65bcfa79639cd84f0fb1a3a5324b91368
Hitesh <hiteshbhatia3559@gmail.com>
2019-03-24 12:21:04 +0530

    Two pair working.

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
-from itertools import chain,combinations,permutations
+from itertools import chain, combinations, permutations
 from alpha_vantage.foreignexchange import ForeignExchange
 from pprint import pprint
 
 API_KEY = '6N6NCHHOY72NXZ4T'
 
+
 def perms(base_pairs):
     perms = []
-    for i in range(1,len(base_pairs)+1):
-        perms.append(list(permutations(base_pairs,i)))
+    for i in range(1, len(base_pairs) + 1):
+        perms.append(list(permutations(base_pairs, i)))
+    perms.pop(0)
     return perms
 
 
+def chain_calculator(chain):
+    chain = list(chain)
+    i = 0
+    net_PNL = float()
+    while i < len(chain):
+        if i != len(chain) - 1:
+            data1, _ = cc.get_currency_exchange_rate(from_currency=chain[i], to_currency=chain[i + 1])
+            data2, _ = cc.get_currency_exchange_rate(from_currency=chain[i + 1], to_currency=chain[i])
+            first_number = round(float(data1['5. Exchange Rate']), 8)
+            second_number = round(1 / float(data2['5. Exchange Rate']), 8)
+            print(first_number / second_number)
+        i += 1
+
+
 base_pairs = ['USD', 'EUR', 'JPY', 'GBP', 'CHF', 'CAD', 'AUD', 'HKD']
-base_perms = perms(base_pairs) # total amount of items in this dataset is 109600...
-base_perms.pop(0)
+base_perms = perms(base_pairs)  # total amount of items in this dataset is 109600...
 # At this point, index 0 contains base pairs permutations of basepairs combination of 2 elements
-# Index 1-8 per index contain a list of chains to be evaluated... maybe just
 cc = ForeignExchange(key=API_KEY)
 
-data1,_ = cc.get_currency_exchange_rate(from_currency='EUR',to_currency='USD')
-data2,_ = cc.get_currency_exchange_rate(from_currency='USD',to_currency='EUR')
-first_number = round(float(data1['5. Exchange Rate']),8)
-second_number = round(1/float(data2['5. Exchange Rate']),8)
-# print(str(data1['5. Exchange Rate'])+" - "+str(round(1/float(data2['5. Exchange Rate']),8)))
-print(first_number/second_number)
-
+chain_calculator(base_perms[0][12])
